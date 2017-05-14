28b04cbec19bd7d4298b09b2d114c6be0ce26a9c
ilian790112 <ilian_790112@abv.bg>
2017-05-14 09:27:01 +0300

    Registration Tests

---
+++ Blog-Skeleton/Blog.UI.Tests/Models/AccessExcelData.cs
@@ -0,0 +1,35 @@
+﻿using Blog.UI.Tests.Models;
+using Configuration;
+using Dapper;
+using System;
+using System.Data.OleDb;
+using System.Linq;
+
+
+namespace Blog.UI.Tests.Models
+{
+    public class AccessExcelData
+    {
+        public static string TestDataFileConnection()
+        {
+            var path = ConfigurationManager.AppSettings["TestDataSheetPath"];
+            var filename = "UserData.xlsx";
+            var con = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;
+		                              Data Source = {0}; 
+		                              Extended Properties=Excel 12.0;", path + filename );
+            return con;
+        }
+
+        public static User GetTestData(string keyName)
+        {
+            using (var connection = new OleDbConnection(TestDataFileConnection()))
+            {
+                connection.Open();
+                var query = string.Format("select * from [DataSet$] where key = '{0}'", keyName);
+                var value = connection.Query<User>(query).FirstOrDefault();
+                connection.Close();
+                return value;
+            }
+        }
+    }
+}
---
+++ Blog-Skeleton/Blog.UI.Tests/RegistrationTests.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using Configuration;
+using Blog.UI.Tests.Pages.RegistrationPage;
+using Blog.UI.Tests.Models;
+
+namespace Blog.UI.Tests
+{
+    [TestFixture]
+    public class UITests
+    {
+
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Init()
+        {
+            this.driver = new ChromeDriver();
+        }
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                string filename = ConfigurationManager.AppSettings["Logs"] + TestContext.CurrentContext.Test.Name + ".txt";
+                if (File.Exists(filename))
+                {
+                    File.Delete(filename);
+                }
+                File.WriteAllText(filename, TestContext.CurrentContext.Test.FullName + "        " + TestContext.CurrentContext.WorkDirectory + "            " + TestContext.CurrentContext.Result.PassCount);
+
+                var screenshot = ((ITakesScreenshot)this.driver).GetScreenshot();
+                screenshot.SaveAsFile(filename + TestContext.CurrentContext.Test.Name + ".jpg", ScreenshotImageFormat.Jpeg);
+            }
+
+            this.driver.Quit();
+
+        }
+
+
+        [Test]
+        public void RegisterWithoutFullName()
+        {
+            var regPage = new RegistrationPage(this.driver);
+            User user = AccessExcelData.GetTestData("RegisterWithoutFullName");
+
+            regPage.NavigateTo();
+            regPage.FillRegistrationForm(user);
+           
+            regPage.AssertErrorMessage("The Full Name field is required.");
+        }
+
+        [Test]
+        public void RegisterWithoutEmail()
+        {
+            var regPage = new RegistrationPage(this.driver);
+            User user = AccessExcelData.GetTestData("RegisterWithoutEmail");
+
+            regPage.NavigateTo();
+            regPage.FillRegistrationForm(user);
+
+            regPage.AssertErrorMessage("The Email field is required.");
+        }
+
+        [Test]
+        public void RegisterWithoutPassword()
+        {
+            var regPage = new RegistrationPage(this.driver);
+            User user = AccessExcelData.GetTestData("RegisterWithoutPassword");
+
+            regPage.NavigateTo();
+            regPage.FillRegistrationForm(user);
+
+            regPage.AssertErrorMessage("The Password field is required.");
+        }
+
+        [Test]
+        public void PasswordsDontMatch()
+        {
+            var regPage = new RegistrationPage(this.driver);
+            User user = AccessExcelData.GetTestData("PasswordsDontMatch");
+
+            regPage.NavigateTo();
+            regPage.FillRegistrationForm(user);
+
+            regPage.AssertErrorMessage("The password and confirmation password do not match.");
+        }
+
+        [Test]
+        public void RegisterWithInvalidEmail()
+        {
+            var regPage = new RegistrationPage(this.driver);
+            User user = AccessExcelData.GetTestData("RegisterWithInvalidEmail");
+
+            regPage.NavigateTo();
+            regPage.FillRegistrationForm(user);
+
+            regPage.AssertErrorMessage("The Email field is not a valid e-mail address.");
+        }
+
+    }
+}
