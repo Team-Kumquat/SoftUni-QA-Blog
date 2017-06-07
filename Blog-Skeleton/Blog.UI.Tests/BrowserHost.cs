b3ac1a2943ef137e56e8e5ac5aae5a1fe4a33c2b
zutobg <zytobg@gmail.com>
2017-06-07 22:52:33 +0300

    Init a Test Project with first smoke test for HomePage loading test with Chrome default

---
+++ Blog-Skeleton/Blog.UI.Tests/BrowserHost.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Chrome;
+using TestStack.Seleno.Configuration;
+
+namespace Blog.UI.Tests
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        private static readonly string RootUrl = "http://localhost:60634/Article/List";
+
+        static BrowserHost()
+        {
+            Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+            // For Firefox: Instance.Run("Blog", 60634);
+            // For Chrome: Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
+            // or Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+
+        }
+    }
+}
---
+++ Blog-Skeleton/Blog.UI.Tests/UITests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.UI.Tests.Pages.HomePage;
+
+namespace Blog.UI.Tests
+{
+    [TestFixture]
+    public class UITests
+    {
+        [Test]
+        public void BlogLogoDisplayedRightMessage()
+        {
+            var homePage = new HomePage(BrowserHost.Instance.Application.Browser);
+
+            homePage.NavigateTo();
+
+            homePage.AssertLogo();
+        }
+    }
+}
