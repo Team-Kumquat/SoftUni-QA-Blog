a476bc79a0838ce59915288c9e146771a3817deb
zutobg <zytobg@gmail.com>
2017-06-08 04:19:43 +0300

    Adding Model for reading data from excel

---
+++ Blog-Skeleton/Blog.UI.Tests/Models/AccessExcelData.cs
@@ -0,0 +1,47 @@
+﻿using Blog.UI.Tests.Models;
+using Configuration;
+using Dapper;
+using System;
+using System.Data.OleDb;
+using System.Linq;
+
+
+namespace Blog.UI.Tests.Models
+{
+    public class AccessExcelData
+    {
+        public static string TestDataFileConnection()
+        {
+            var path = ConfigurationManager.AppSettings["TestDataSheetPath"];
+            var filename = "UserData.xlsx";
+            var con = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;
+		                              Data Source = {0}; 
+		                              Extended Properties=Excel 12.0;", path + filename);
+            return con;
+        }
+
+        public static User GetUserTestData(string keyName)
+        {
+            using (var connection = new OleDbConnection(TestDataFileConnection()))
+            {
+                connection.Open();
+                var query = string.Format("select * from [UserDataSet$] where key = '{0}'", keyName);
+                var value = connection.Query<User>(query).FirstOrDefault();
+                connection.Close();
+                return value;
+            }
+        }
+
+        public static Post GetPostTestData(string keyName)
+        {
+            using (var connection = new OleDbConnection(TestDataFileConnection()))
+            {
+                connection.Open();
+                var query = string.Format("select * from [PostDataSet$] where key = '{0}'", keyName);
+                var value = connection.Query<Post>(query).FirstOrDefault();
+                connection.Close();
+                return value;
+            }
+        }
+    }
+}
