4d7f1e65d9d4815384bf6fcc4bd7396a43800f47
zutobg <zytobg@gmail.com>
2017-06-08 04:25:25 +0300

    removing a dependency causing an error when test is executed

--- Blog-Skeleton/Blog.UI.Tests/Models/AccessExcelData.cs
+++ Blog-Skeleton/Blog.UI.Tests/Models/AccessExcelData.cs
@@ -1,5 +1,5 @@
 ﻿using Blog.UI.Tests.Models;
-using Configuration;
+using System.Configuration;
 using Dapper;
 using System;
 using System.Data.OleDb;
@@ -19,29 +19,6 @@ namespace Blog.UI.Tests.Models
 		                              Extended Properties=Excel 12.0;", path + filename);
             return con;
         }
-
-        public static User GetUserTestData(string keyName)
-        {
-            using (var connection = new OleDbConnection(TestDataFileConnection()))
-            {
-                connection.Open();
-                var query = string.Format("select * from [UserDataSet$] where key = '{0}'", keyName);
-                var value = connection.Query<User>(query).FirstOrDefault();
-                connection.Close();
-                return value;
-            }
-        }
-
-        public static Post GetPostTestData(string keyName)
-        {
-            using (var connection = new OleDbConnection(TestDataFileConnection()))
-            {
-                connection.Open();
-                var query = string.Format("select * from [PostDataSet$] where key = '{0}'", keyName);
-                var value = connection.Query<Post>(query).FirstOrDefault();
-                connection.Close();
-                return value;
-            }
-        }
+    
     }
 }
