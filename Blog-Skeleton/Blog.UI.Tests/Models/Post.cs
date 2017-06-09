bb3fc24c7d26e45caec98b2ee83cb2e5abb6822f
zutobg <zytobg@gmail.com>
2017-06-09 18:55:36 +0300

    adding other pages to POM

--- Blog-Skeleton/Blog.UI.Tests/Models/Post.cs
+++ Blog-Skeleton/Blog.UI.Tests/Models/Post.cs
@@ -3,8 +3,9 @@
     public class Post
     {
 
-        private string title;
-        private string content;
+        //default values in case setter are omitted by the data access layer
+        private string title = "";
+        private string content = "";
 
 
         public Post()
